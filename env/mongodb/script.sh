dcc0567d140974750a31f99ba4cdcc1efa3f0db4
OLIVER TAYLOR <olivertaylor1295@hotmail.com>
2023-01-31 15:15:19 +0000

    running

--- env/mongodb/script.sh
+++ env/mongodb/script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+echo hello
 echo -------------- Import the public key for APT ---------------
 sudo wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 
 echo ---------------- Create list file for MongoDV --------------
-sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
 echo ------------------------ Update packages -------------------
-sudo apt-get update
+sudo apt-get update -y
 
 echo ------------------------ Install Mongo ---------------------
 sudo apt-get install -y mongodb-org
@@ -16,10 +17,12 @@ echo --------------- Remove Exsiting Conf File ------------------
 sudo rm /etc/mongod.conf
 
 echo ---------------------- Copy New Conf File ------------------
-sudo ln -s /home/vagrant/env/mongodb/mongod.conf /etc/mongod.conf
+sudo ln -s /home/ubuntu/env/mongodb/mongod.conf /etc/mongod.conf
 
 echo ------------------------- Start Mongo ----------------------
 sudo systemctl start mongod
+sudo systemctl enable mongod
 
 echo ------------------------- Get Mongo Status -----------------
 sudo systemctl status mongod
+
